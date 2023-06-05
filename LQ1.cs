39dd99170820e0e7ac87fb807cebceb3a4c94cbb
Rage <rageevgeniy@gmail.com>
2023-06-05 15:10:22 +0500

    upload: seminartasks

---
+++ LQ1.cs
@@ -0,0 +1,29 @@
+﻿//LQ1.Используя Linq, циклически сдвинуть массив на K влево. Без циклов. Без Reverse.
+
+using System;
+using System.Linq;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        Console.WriteLine("Введите количество сдвигов: ");
+        int k = int.Parse(Console.ReadLine());
+        int[] shiftedArray = ShiftArrayLeft(array, k);
+
+        Console.WriteLine($"Сдвиг на: {k}");
+        Console.WriteLine(string.Join(", ", shiftedArray));
+    }
+    static int[] ShiftArrayLeft(int[] array, int k)
+    {
+        int n = array.Length;
+        // Корректируем k так, чтобы оно находилось в диапазоне длины массива
+        k = k % n;
+        // Используем Skip(k) для пропуска первых k элементов массива и Concat с Take для добавления первых k элементов в конец.
+        // Результат записываем в массив с помощью ToArray().
+        var shiftedArray = array.Skip(k).Concat(array.Take(k)).ToArray();
+        return shiftedArray;
+    }
+}
---
+++ LQ3.cs
@@ -0,0 +1,55 @@
+﻿//LQ3 Дан набор строк. Найти все строки, в которых ни одна буква не повторяется больше 2 раз.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Line
+{public string Text { get; set; }}
+
+public class LineChecker
+{
+    //Проводим итерации по каждой строке и проверяем кол-во каждой буквы слова по словарю.
+    public IEnumerable<Line> FindLinesWithNoLetterRepeatedMoreThanTwice(IEnumerable<Line> lines)
+    {
+        var result = new List<Line>();
+
+        foreach (var line in lines)
+        {
+            var charCount = new Dictionary<char, int>();
+
+            foreach (var c in line.Text)
+            {
+                if (charCount.ContainsKey(c)) charCount[c]++;
+                else charCount[c] = 1;
+            }
+            //Если буква не имеет счетчика больше 2(т.е соответствует условию), включаем её в результат.
+            if (charCount.Values.All(count => count <= 2)) result.Add(line);
+        }
+
+        return result;
+    }
+}
+
+public class LQ3
+{
+    public static void Main(string[] args)
+    { //Создаем объекты для проверки роботоспособности программы
+        var lines = new[]
+        {
+            new Line { Text = "тест номер один" },
+            new Line { Text = "саспенс "},
+            new Line { Text = "панорама" },
+            new Line { Text = "тест последний" },
+        };
+
+        var lineChecker = new LineChecker();
+        var result = lineChecker.FindLinesWithNoLetterRepeatedMoreThanTwice(lines);
+
+        Console.WriteLine("строки, в которых ни одна буква не повторяется больше 2 раз:");
+        foreach (var line in result)
+        {
+            Console.WriteLine(line.Text);
+        }
+    }
+}
